0a89af00b495df2ef938c1fff141d6f178689711
verma0073 <32905810+verma0073@users.noreply.github.com>
2023-06-02 19:06:42 +0530

    Update c1-versions.tf

--- c1-versions.tf
+++ c1-versions.tf
@@ -11,8 +11,8 @@ terraform {
 
 provider "aws" {
   region = var.aws_region
-  access_key = "AKIAQU2NOGZ5HIRRSIHT"
-  secret_key = "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"
+  ###access_key = "AKIAQU2NOGZ5HIRRSIHT"
+  ####secret_key = "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"
 }
 
 
